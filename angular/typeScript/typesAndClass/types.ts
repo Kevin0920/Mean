634153c9ed471ea3509006bdf4f2af61dd16ca79
Kevin Ke <kevinlingsike@gmail.com>
2017-07-17 17:19:45 -0700

    dojoMail

--- angular/typeScript/typesAndClass/types.ts
+++ angular/typeScript/typesAndClass/types.ts
@@ -1,23 +1,3 @@
-
-MyNode = (function () {
-    function MyNode(val) {
-        this.val = 0;
-        this.val = val;
-    }
-    MyNode.prototype.doSomething = function () {
-        this._priv = 10;
-    };
-    return MyNode;
-}());
-myNodeInstnace = new MyNode(1);
-console.log(myNodeInstnace.val);
-
-function myFunction() {
-    console.log("Hello World");
-    return;
-}
-
-
 let myNum: number = 5;
 let myString: string = "Hello Universe";
 let myArr: number[]=[1,2,3,4];
@@ -34,24 +14,25 @@ function sendingErrors(): never{
     throw new Error('Error message');
 }
 
+let myNodeInstnace = new MyNode(1);
+console.log(myNodeInstnace.val);
 function myFunction(): void{
     console.log('Hello World');
     return;
+
+
+
 }
-
-
-
 class MyNode {
     val: number;
-
-    constructor(valueP: number, ){
+    _priv: number;
+    constructor(val: number) {
         this.val = 0;
+        this.val = val;
     }
-    doSomethingFun(){
+    doSomething() {
         this._priv = 10;
     }
-}
-let firstSLNode = new MyNode(1)
-console.log(myNodeinstace.val);
+};
 
 
